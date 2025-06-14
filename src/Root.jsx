cf4ac94805869c632e299204dfa620934c9283bc
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-06-14 03:01:18 +0300

    revert to BrowserRouter

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './converter/App';
 import Landing from './landing/Landing';
 import Header from './common/Header';
